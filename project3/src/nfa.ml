8c54ed47f5934253ba8f7b5be5c1bbd2a36630f0
azwod <70917200+azwod@users.noreply.github.com>
2020-10-28 17:46:06 -0400

    Update nfa.ml

--- project3/src/nfa.ml
+++ project3/src/nfa.ml
@@ -37,8 +37,8 @@ let move (nfa: ('q,'s) nfa_t) (qs: 'q list) (s: 's option) : 'q list =
 
 let e_closure (nfa: ('q,'s) nfa_t) (qs: 'q list) : 'q list =
   let f a b = (match b with
-              | (x,y,z) -> if List.mem x qs then z::a else a)
-                                              in  List.fold_left f [] nfa.delta
+              | (x,y,z) -> if List.mem x qs then (if y=nil then x::z::a else x::a) else a)
+                                              in  List.fold_left f qs nfa.delta
 
 let accept (nfa: ('q,char) nfa_t) (s: string) : bool =
   failwith "unimplemented"
