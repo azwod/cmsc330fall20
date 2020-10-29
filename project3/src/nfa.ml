48c13b41e7bf0236894896f6b1acbc6e413bb842
azwod <70917200+azwod@users.noreply.github.com>
2020-10-28 20:51:10 -0400

    Update nfa.ml

--- project3/src/nfa.ml
+++ project3/src/nfa.ml
@@ -48,7 +48,7 @@ let explode (s: string) : char list =
   let rec accept_helper (nfa: ('q,char) nfa_t) (c: char list) : bool =
     match c with
     | h::t -> (match (nfa.delta) with
-               | (x,y,z) -> if y = Some h then accept_helper nfa t else (if y = None then accept_helper nfa t else false))
+               | (x,z) -> if z =  h then accept_helper nfa t else (if x = None then accept_helper nfa t else false))
     | _ -> true
 
   let accept (nfa: ('q,char) nfa_t) (s: string) : bool =
