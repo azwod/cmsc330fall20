beed25dbf5e7eddd6109102a219e98ab7d3b22ef
azwod <70917200+azwod@users.noreply.github.com>
2020-10-29 23:53:34 -0400

    Update nfa.ml

--- project3/src/nfa.ml
+++ project3/src/nfa.ml
@@ -32,7 +32,7 @@ let explode (s: string) : char list =
 
   let move (nfa: ('q,'s) nfa_t) (qs: 'q list) (s: 's option) : 'q list =
     let f a b = (match b with
-                | (x,y,z) -> if List.mem x qs && y=s then (if List.mem z a then a else z::a) else a
+                | (x,y,z) -> if List.mem x qs && y=s then (if List.mem z a then a else z::a) else a)
                                                 in  List.fold_left f [] nfa.delta
 
   (*let e_closure (nfa: ('q,'s) nfa_t) (qs: 'q list) : 'q list =
